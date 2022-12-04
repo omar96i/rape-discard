1023c7f44144b09385cec7323402bdae52e3f80a
omar96i <sagara9930@hotmail.com>
2022-12-04 17:38:41 -0500

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,18 +18,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call([
-            AlimentoSeeder::class,
-            DepartamentoSeeder::class,
-            MunicipioSeeder::class
-        ]);
-        // DB::table('users')->insert([
-		// 	'email' => 'admin@gmail.com',
-		// 	'name' => 'admin',
-		// 	'password' => bcrypt('12345678'),
-		// 	'created_at' => \Carbon\Carbon::now()->toDateTimeString(),
-		// 	'updated_at' => \Carbon\Carbon::now()->toDateTimeString(),
-		// ]);
+        // $this->call([
+        //     AlimentoSeeder::class,
+        //     DepartamentoSeeder::class,
+        //     MunicipioSeeder::class
+        // ]);
+        DB::table('users')->insert([
+			'email' => 'admin@gmail.com',
+			'name' => 'admin',
+			'password' => bcrypt('12345678'),
+			'created_at' => \Carbon\Carbon::now()->toDateTimeString(),
+			'updated_at' => \Carbon\Carbon::now()->toDateTimeString(),
+		]);
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
