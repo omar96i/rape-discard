df650fb7195250cb8ce5a4f7b270ce28410aac50
omar96i <sagara9930@hotmail.com>
2023-03-28 07:51:15 -0500

    last update

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,8 +1,8 @@
 <template>
     Bievenido!
     <label class="form-label btn btn-primary" for="import_excel">Selecciona el archivo a importar</label>
-    <input type="file" @change="onInputExcelChange" id="import_excel" style="display:none">
-    <button class="btn btn-success" @click="importExcel()">Importar</button>
+    <!-- <input type="file" @change="onInputExcelChange" id="import_excel" style="display:none">
+    <button class="btn btn-success" @click="importExcel()">Importar</button> -->
 </template>
 
 <script>
